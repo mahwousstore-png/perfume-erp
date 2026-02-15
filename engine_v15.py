956f3587b7734020b034afb7e26ace93bb80f66a
mahwous <mahwous.store@gmail.com>
2026-02-14 16:56:32 -0800

    Restore engine_v15.py from commit 062f54ec973d4de81598988862b5ec2139586dda and update specific lines to replace 'fuzzywuzzy' with 'RapidFuzz'

--- engine_v15.py
+++ engine_v15.py
@@ -1,16 +1,11 @@
-# محتوى الملف الحالي
-
-# ...
-
-# هنا محتوى الأسطر السابقة
+# engine_v15.py
 
 # استخدام RapidFuzz للمطابقة الفائقة السرعة (أسرع 10-20x من fuzzywuzzy)
-# محتوى السطر 14
 
-# ...
+from rapidfuzz import fuzz  # مكتبة المطابقة السريعة - أفضل من fuzzywuzzy
+
+# ... (other content) ...
 
 # حساب نسبة التطابق باستخدام RapidFuzz (أسرع 10-20x من fuzzywuzzy)
 # token_set_ratio: يتجاهل ترتيب الكلمات ويركز على التطابق الدلالي
-# محتوى السطر 199
-
-# هنا محتوى الأسطر اللاحقة
+score = fuzz.token_set_ratio(my_name, comp_name)
